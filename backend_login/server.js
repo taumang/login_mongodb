d2a12c23684248f1a84338955963529a87444f92
Your Name <09taumang@gmail.com>
2021-02-28 00:25:54 +0200

    nothing happend here

--- backend_login/server.js
+++ backend_login/server.js
@@ -9,7 +9,7 @@ dotenv.config()
 
 mongoose.connect(process.env.DATABASE_ACCESS,()=> console.log("Database is connected"))
 
-app.use(express.json())
-app.use(cors())
-app.use('/app', routesUrls)
+app.use(express.json());
+app.use(cors());
+app.use('/app', routesUrls);
 app.listen(4000,()=> console.log("server is running"))
